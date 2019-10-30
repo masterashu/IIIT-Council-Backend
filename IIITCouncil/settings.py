1428d8627d2801c8ddc1818bab1fdd2e2bfa5a9a
Ashutosh Chauhan <masterashu@live.in>
2019-10-30 21:54:44 +0530

    Update DB SETTINGS to postgres

--- IIITCouncil/settings.py
+++ IIITCouncil/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django.db.backends.postgresql_psycopg2
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'IIITCouncil.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
