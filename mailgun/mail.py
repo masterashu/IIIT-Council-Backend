249b4fc7986287ed69913fe68be91fc1e2bede84
Ashutosh Chauhan <masterashu@live.in>
2019-11-02 00:10:28 +0530

    adding fail_silently kwargs to send_mail

--- mailgun/mail.py
+++ mailgun/mail.py
@@ -2,8 +2,9 @@ import requests
 from django.conf import settings
 
 
-def send_mail(subject, body, sender, to):
-    return requests.post(
+def send_mail(subject, body, sender, to, fail_silently=True):
+    # Post a request to MailGun API, It will process it and mail it through it's system
+    response =  requests.post(
         settings.MAILGUN_BASE_URL,
         auth=('api', settings.MAILGUN_API_KEY),
         data={'from': sender,
@@ -11,11 +12,16 @@ def send_mail(subject, body, sender, to):
               'subject': subject,
               'text': body
               })
+    # If Response is not OK(200) show failed message
+    if not fail_silently and response.status_code != 200:
+        print("Failed to send mail to ", to, "\nSubject: ", subject, "\nBody: ", body)
+        # TODO raise any error or log failed email.
+    return response
 
-def send_mass_mail(*args):
-    for mail in args:
+def send_mass_mail(datatuples, fail_silently=True):
+    for mail in datatuples:
         try:
             subject, body, sender, to = mail
-            yield(send_mail(subject, body, sender, to))
+            yield send_mail(subject, body, sender, to, fail_silently=fail_silently)
         except ValueError as e:
             print("Not Enough Values provided, expected 4")
