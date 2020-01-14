47a72533833bed3b94304a5321641a03a81b10bf
Ashutosh Chauhan <masterashu@live.in>
2020-01-14 18:25:11 +0530

    integrating urls in base, home, about templates

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from base.views import home_view, about_view, about_iiit_act_view, about_ppp_partners_view, \
+    about_system_review_reports_view, about_administrative_structure_view, about_history_view
 
 urlpatterns = [
-    # path('', home_view, name='home_page'),
-    # path('about', about_view, name='about'),
-    # path('about/iiit_act', about_view, name='about_iiit_act'),
-    # path('about/history', about_view, name='about_history'),
-    # path('about/administrative_structure', about_view, name='about_administrative_structure'),
-    # path('about/system_review_reports', about_view, name='about_system_review_reports'),
-    # path('about/ppp_partners', about_view, name='about_ppp_partners'),
+    path('', home_view, name='home_page'),
+    path('about', about_view, name='about'),
+    path('about/iiit_act', about_iiit_act_view, name='about_iiit_act'),
+    path('about/history', about_history_view, name='about_history'),
+    path('about/administrative_structure', about_administrative_structure_view,
+         name='about_administrative_structure'),
+    path('about/system_review_reports', about_system_review_reports_view,
+         name='about_system_review_reports'),
+    path('about/ppp_partners', about_ppp_partners_view, name='about_ppp_partners'),
 ]
