b918cb11fe141e9e63165bcb3f3db8a232475cc7
Ashutosh Chauhan <masterashu@live.in>
2020-01-14 15:42:23 +0530

    add login logout urls

--- IIITCouncil/settings.py
+++ IIITCouncil/settings.py
@@ -189,3 +189,7 @@ AUTH_USER_MODEL = 'registration.User'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- base/urls.py
+++ base/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     # path('about/administrative_structure', about_view, name='about_administrative_structure'),
     # path('about/system_review_reports', about_view, name='about_system_review_reports'),
     # path('about/ppp_partners', about_view, name='about_ppp_partners'),
-
 ]
