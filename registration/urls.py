f944d9ae9f249176591faeb009eb4ec359f35bd6
Ashutosh Chauhan <masterashu@live.in>
2020-01-15 20:09:41 +0530

    adding password reset views

--- registration/urls.py
+++ registration/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
-from registration.views import LoginView, LogoutView, PasswordResetView as PRV, PasswordResetDoneView as PRDV
+from registration.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
-
-from registration.views import PasswordChangeView
-from registration.views import LoginView, LogoutView, PasswordResetView, PasswordResetDoneView
-from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView, PasswordResetConfirmView
 
 
 urlpatterns = [
@@ -12,17 +8,9 @@ urlpatterns = [
     path('logout', LogoutView.as_view(), name='logout'),
     path('password-change/', PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/', PRV.as_view(), name='password_reset'),
-    path('password-reset/done/', PRDV.as_view(), name='password_reset_done'),
-    # path('password-change/', PasswordChangeView.as_view(), name='password_change'),
-    # path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    # path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
-    # path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    # path('reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
     path('password-reset/', PasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 ]
+
--- registration/views.py
+++ registration/views.py
@@ -4,11 +4,11 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 from django.contrib.auth.mixins import LoginRequiredMixin
 from registration.models import User
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode as b64_encode
+from django.utils.http import urlsafe_base64_encode as b64_encode, urlsafe_base64_decode as b64_decode
 from mailgun.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
-from django.utils.html import strip_tags
+from django.http import HttpResponse
 
 
 class LoginView(View):
@@ -48,7 +48,7 @@ class PasswordChangeView(LoginRequiredMixin, View):
 
     def post(self, request):
         old_pass, new_pass, conf_new_pass = request.POST[
-            'old'], request.POST['new'], request.POST['confirm_new']
+                                                'old'], request.POST['new'], request.POST['confirm_new']
 
         if request.user.check_password(old_pass):
             if new_pass == conf_new_pass:
@@ -99,10 +99,32 @@ class PasswordResetDoneView(View):
 
 class PasswordResetConfirmView(View):
     def get(self, request, uidb64, token):
-
-        pass
+        print(token)
+        dtg = default_token_generator
+        try:
+            user = User.objects.get(pk=b64_decode(uidb64).decode())
+        except:
+            return HttpResponse(status=400, content='Bad URL.')
+        if dtg.check_token(user, token):
+            return render(request, 'registration/password_reset_confirm.html', context={'uid': uidb64, 'token': token})
+        return HttpResponse(status=400, content='Bad URL.')
 
 
 class PasswordResetCompleteView(View):
     def post(self, request):
-        pass
+        try:
+            uidb64 = request.POST['uid']
+            token = request.POST['token']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+            user = User.objects.get(pk=b64_decode(uidb64).decode())
+            if password1 != password2:
+                raise ValueError
+        except KeyError:
+            return HttpResponse(status=400, content="Bad Request. Please verify passwords or try again")
+        except ValueError:
+            return HttpResponse(status=400, content="Bad Request. Passwords do not match.")
+        except User.DoesNotExist:
+            return HttpResponse(status=400, content="Bad Request.")
+        user.set_password(password1)
+        return render(request, 'registration/password_reset_complete.html')
