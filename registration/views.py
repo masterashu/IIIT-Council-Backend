09dda74a0654e103af0aec631ddf4adaade0aa90
Ketan Lambat <ketan.lambat@ymail.com>
2020-01-16 09:24:48 +0530

    login html moved to base dir

--- registration/views.py
+++ registration/views.py
@@ -7,30 +7,28 @@ from django.contrib.auth import authenticate, login as auth_login, logout as aut
 
 
 class LoginView(View):
-    def get(self, request):
-        return render(request, 'login/registration/login.html')
+  def get(self, request):
+    return render(request, 'registration/login.html')
 
-    def post(self, request):
-        username, password = request.POST['username'], request.POST['password']
-        user = authenticate(request, username=username, password=password)
+  def post(self, request):
+    username, password = request.POST['username'], request.POST['password']
+    user = authenticate(request, username=username, password=password)
 
-        if user is None:
-            error = 'Unable to login, please verify username and password.'
-            return render(request, 'login/registration/login.html', context={'error': error})
+    if user is None:
+      error = 'Unable to login, please verify username and password.'
+      return render(request, 'registration/login.html', context={'error': error})
 
-        try:
-            auth_login(request, user=user)
-        except:
-            error = 'Unable to login. Please try again in some time.'
-            return render(request, 'base/login.html', context={'error': error})
-        if request.POST['next'] is not None:
-            return redirect(to=request.POST['next'])
-        return redirect(to='home')
+    try:
+      auth_login(request, user=user)
+    except:
+      error = 'Unable to login. Please try again in some time.'
+      return render(request, 'registration/login.html', context={'error': error})
+    if request.POST['next'] is not None:
+      return redirect(to=request.POST['next'])
+    return redirect(to='home')
 
 
 class LogoutView(View):
-    def get(self, request):
-        auth_logout(request)
-        return redirect(to='home')
-
-
+  def get(self, request):
+    auth_logout(request)
+    return redirect(to='home')
