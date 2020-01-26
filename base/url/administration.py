042c9ae536ce81c2e8952d1f9b45f1ad83c594f8
Ashutosh Chauhan <masterashu@live.in>
2020-01-26 20:39:29 +0530

    updating base

--- base/url/administration.py
+++ base/url/administration.py
@@ -3,6 +3,6 @@ from ..view.administration import *
 
 urlpatterns = [
   path('directors', admistr_directors_view, name='admistr_directors'),
-  path('deans', adminstr_deans_view, name='adminstr_deans'),
+  path('deans', adminstr_deans_view, name='admistr_deans'),
   path('registrars', admistr_registrars_view, name='admistr_registrars'),
 ]
--- base/view/administration.py
+++ base/view/administration.py
@@ -5,7 +5,7 @@ def admistr_directors_view(request):
     return render(request, 'administration/directors.html', context={'active': 'admistr_directors'})
 
 def adminstr_deans_view(request):
-    return render(request, 'administration/deans.html', context={'active': 'adminstr_deans'})
+    return render(request, 'administration/deans.html', context={'active': 'admistr_deans'})
 
 def admistr_registrars_view(request):
     return render(request, 'administration/registrars.html', context={'active': 'admistr_registrars'})
