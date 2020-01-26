e58f803f28eca908520134d35f35af0e4bf64c6b
Ashutosh Chauhan <masterashu@live.in>
2020-01-26 20:54:30 +0530

    adding collab links

--- base/url/collaboration.py
+++ base/url/collaboration.py
@@ -2,6 +2,6 @@ from django.urls import path
 from ..view.collaboration import *
 
 urlpatterns = [
-  path('MoU', collab_MoU_view, name='collab_MoU'),
+  path('mou', collab_mou_view, name='collab_mou'),
   path('student_exchange', collab_student_exchange_view, name='collab_student_exchange'),
 ]
--- base/view/collaboration.py
+++ base/view/collaboration.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 
 
-def collab_MoU_view(request):
-    return render(request, 'collaboration/MoU.html', context={'active': 'collab_MoU'})
+def collab_mou_view(request):
+    return render(request, 'collaboration/MoU.html', context={'active': 'collab_mou'})
+
 
 def collab_student_exchange_view(request):
     return render(request, 'collaboration/student_exchange.html', context={'active': 'collab_student_exchange'})
