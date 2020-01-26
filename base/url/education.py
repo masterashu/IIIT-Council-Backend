94a1b7b0feaa8ff126e76d9ac336bdc584f872c8
Ashutosh Chauhan <masterashu@live.in>
2020-01-26 17:29:31 +0530

    add side nav education

--- base/url/education.py
+++ base/url/education.py
@@ -2,5 +2,8 @@ from django.urls import path
 from ..view.education import *
 
 urlpatterns = [
-    path('btech', education_btech, name='education_btech'),
+    path('btech', education_btech, name='education_b_tech'),
+    path('mtech', education_m_tech, name='education_m_tech'),
+    path('ms', education_m_s, name='education_m_s'),
+    path('phd', education_ph_d, name='education_p_hd'),
 ]
