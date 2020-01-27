d65576fb905aecb44619f6a45ed4c053b557c148
Ketan Lambat <ketan.lambat@ymail.com>
2020-01-27 14:20:19 +0530

    pdf link added in minutes of meeting

--- base/view/collaboration.py
+++ base/view/collaboration.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 
 
+
 def collab_mou_view(request):
-    return render(request, 'collaboration/MoU.html', context={'active': 'collab_mou'})
+  return render(request, 'collaboration/MoU.html', context={'active': 'collab_mou'})
 
 
 def collab_student_exchange_view(request):
-    return render(request, 'collaboration/student_exchange.html', context={'active': 'collab_student_exchange'})
+  return render(request, 'collaboration/student_exchange.html', context={'active': 'collab_student_exchange'})
+
--- base/view/forum.py
+++ base/view/forum.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 
-
 def forum_chairman_view(request):
     return render(request, 'coordination_forum/chairman.html', context={'active': 'forum_chairman'})
 
@@ -15,3 +14,4 @@ def forum_secretary_view(request):
 
 def forum_meeting_minutes_view(request):
     return render(request, 'coordination_forum/meeting_minutes.html', context={'active': 'forum_meeting_minutes'})
+
